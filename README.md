5f4e54e024eb96492f16091a89d3ffaf00d1c593
Malolan <malolan@growfin.ai>
2021-06-06 14:25:14 +0530

    misc changes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # emoji-data
 Builds emoji data for consumption
+
+- You can find the JSON in `build` directory
+- Types are specified in types.d.ts (`GrandEmoji`)
--- main.ts
+++ main.ts
@@ -1,7 +1,7 @@
 import { buildEmoji, buildGroupedEmoji } from './build-emoji.ts'
 
 const data = await buildEmoji()
-const [groupedData, ungroupedEmojis] = buildGroupedEmoji(data)
+const [groupedData, _] = buildGroupedEmoji(data)
 
 await Deno.writeTextFile('./build/emoji-list.json', JSON.stringify(data))
 console.log('data written to ./build/emoji-list.json')
